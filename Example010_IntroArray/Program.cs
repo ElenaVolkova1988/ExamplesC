c9a08b6eca9756cf0d3d763e820077cd17cbd67d
Elena <xoroshj_chelovek@mail.ru>
2022-10-06 16:21:31 +0300

    внесла правки

--- Example010_IntroArray/Program.cs
+++ Example010_IntroArray/Program.cs
@@ -1,4 +1,4 @@
-﻿int [] array ={1, 12, 31, 4, 15, 16, 17, 18};
+﻿/*int [] array ={1, 12, 31, 4, 15, 16, 17, 18};
 
 int n = array.Length;
 int find =4;
@@ -11,3 +11,33 @@ while (index<n)
     }
      index++;
 }
+*/
+
+void FillArray(int[] collection)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index]=new Random().Next(1,10);
+        index ++;
+    }
+}
+
+void PrintArray (int[] col)
+{
+    int count = col.Length;
+    int position =0;
+    while (position< count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+FillArray(array);
+PrintArray (array);
+
+
+
